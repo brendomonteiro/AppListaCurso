f88703fd17b06b20cb6c041865ae09d1b40fc57f
brendomonteiro <brendomonteirodasilva@gmail.com>
2023-06-05 15:42:07 -0300

    Initial commit

---
+++ app/src/main/java/devandroid/brendo/applistacurso/MainActivity.java
@@ -0,0 +1,14 @@
+package devandroid.brendo.applistacurso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
